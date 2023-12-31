489c3d3c5c9bf53ef5b93bf5da0b0c3a816b713c
JF <seal_bell@protonmail.com>
2024-01-01 02:11:31 +0800

    删除原有的qt项目，重新测试一个

--- TianJi_Player/lyricsmanager.h
+++ TianJi_Player/lyricsmanager.h
@@ -1,11 +1,25 @@
 #ifndef LYRICSMANAGER_H
 #define LYRICSMANAGER_H
 
+#include <QObject>
+#include <QString>
 
-class LyricsManager
-{
+//- 定义处理歌词文件的类。
+
+class LyricsManager : public QObject {
+    Q_OBJECT
+
 public:
-    LyricsManager();
+    explicit LyricsManager(QObject *parent = nullptr);
+    void loadLyricsForSong(const QString &songPath);
+    QString getCurrentLyrics() const;
+
+signals:
+    void lyricsChanged(const QString &currentLyrics);
+
+private:
+    QString parseLyricsFile(const QString &lyricsPath);
+    QString currentLyrics;
 };
 
 #endif // LYRICSMANAGER_H
--- TianJi_Player/playercontroller.h
+++ TianJi_Player/playercontroller.h
@@ -2,15 +2,27 @@
 #define PLAYERCONTROLLER_H
 
 #include <QObject>
+#include <QProcess>
 
-class PlayerController : public QObject
-{
+//- 定义控制音乐播放（调用 mplayer）的类。
+
+class PlayerController : public QObject {
     Q_OBJECT
+
 public:
     explicit PlayerController(QObject *parent = nullptr);
+    ~PlayerController();
 
-signals:
+    void play(const QString &filePath);
+    void pause();
+    void stop();
+    void next();
+    void previous();
+    void setVolume(int volume);
+    void setPosition(int seconds);
 
+private:
+    QProcess *mplayerProcess;
 };
 
 #endif // PLAYERCONTROLLER_H
